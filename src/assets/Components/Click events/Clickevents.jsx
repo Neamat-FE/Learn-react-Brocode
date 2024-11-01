9b7615e173d22701fde7b7a89a7ee196225e3939
neamat-fe <neamat@flightexperte.com>
2024-11-02 00:56:31 +0600

    clickevent

--- src/assets/Components/Click events/Clickevents.jsx
+++ src/assets/Components/Click events/Clickevents.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Clickevents() {
-  return <div className="container">Clickevents</div>;
+  let count = 0;
+  const handleCLick = (name) => {
+    if (count <= 3) {
+      console.log(`${name} You Clicked ${count} Times.`);
+      count++;
+    } else {
+      console.log(`${name} Stop Clicking Me..`);
+    }
+  };
+  return (
+    <div className="container">
+      <button onClick={() => handleCLick("Nemat")}>Click Here</button>
+    </div>
+  );
 }
 
 export default Clickevents;
